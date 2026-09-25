1b4fa0b7fdb29dab99efdb40b53f75de313e9d17
Benjamin Osahon <ayerebenjaminosahon@gmail.com>
2026-09-25 21:10:14 +0100

    Fix build: drop clsx and tailwind-merge from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,7 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: Array<string | false | null | undefined>) {
+  return inputs.filter(Boolean).join(" ");
 }
 
-export function formatNaira(amount: number) {
-  return new Intl.NumberFormat("en-NG", {
-    style: "currency",
-    currency: "NGN",
-    maximumFractionDigits: 0,
-  }).format(amount);
+export function formatNaira(n: number) {
+  return new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN", maximumFractionDigits: 0 }).format(n);
 }
